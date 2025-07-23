b00a91dc6308aa36d3626c3a304a785f141d06a4
dialga-cmd <adityaraj1234@duck.com>
2025-07-23 10:46:26 +0000

    sasa

--- backend/council.php
+++ backend/council.php
@@ -1,7 +1,10 @@
 <?php
 require_once __DIR__ . '/config.php';
 $db = $conn;
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+header('Content-Type: application/json');
 $action = $_GET['action'] ?? $_POST['action'] ?? '';
 
 switch ($action) {
--- backend/participants.php
+++ backend/participants.php
@@ -1,7 +1,10 @@
 <?php
 require_once __DIR__ . '/config.php';
 header('Content-Type: application/json');
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+header('Content-Type: application/json');
 $db = $conn;
 $action = $_GET['action'] ?? $_POST['action'] ?? '';
 
